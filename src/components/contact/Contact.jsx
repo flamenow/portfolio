72ea45f7d334e941c917d5bcba22cf2599517c11
flamenow <marcosmujica3@gmail.com>
2022-05-14 07:50:45 -0300

    Finishing the project, now  I need to customize!

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,90 @@
 import React from "react";
 import "./contact.css";
+import { AiOutlineMail } from "react-icons/ai";
+import { RiMessengerLine } from "react-icons/ri";
+import { BsWhatsapp } from "react-icons/bs";
+import { useRef } from "react";
+import emailjs from "emailjs-com";
 const Contact = () => {
-  return <section id="contact">Contact</section>;
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_fcst0s4",
+        "template_zmpeni4",
+        form.current,
+        "vCL9LvbCLALmESCTl"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  };
+  return (
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <AiOutlineMail className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>marcosmujica3@gmail.com</h5>
+            <a href="mailto:marcosmujica3@gmail.com" target="_blank">
+              Send a message
+            </a>
+          </article>
+
+          <article className="contact__option">
+            <RiMessengerLine className="contact__option-icon" />
+            <h4>Messenger</h4>
+            <h5>Marcos Mujica</h5>
+            <a href="https://m.me/marcos.mujicamachado" target="_blank">
+              Send a message
+            </a>
+          </article>
+
+          <article className="contact__option">
+            <BsWhatsapp className="contact__option-icon" />
+            <h4>WhatsApp</h4>
+            <h5>+55 53 9 9988-8658</h5>
+            <a
+              href="https://api.whatsapp.com/send?phone=5553999888658"
+              target="_blank"
+            >
+              Send me a message!
+            </a>
+          </article>
+        </div>
+        {/* END OF CONTACTS OPTIONS*/}
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,55 @@
 import React from "react";
 import "./footer.css";
+import { GrFacebookOption } from "react-icons/gr";
+import { FiInstagram } from "react-icons/fi";
+import { BsGithub } from "react-icons/bs";
 const Footer = () => {
-  return <section id="footer">Footer</section>;
+  return (
+    <footer>
+      <a href="#" className="footer__logo">
+        Flame NoW
+      </a>
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      <div className="footer__socials">
+        <a href="https://facebook.com">
+          <GrFacebookOption />
+        </a>
+        <a href="https://instagram.com">
+          <FiInstagram />
+        </a>
+        <a href="https://github.com">
+          <BsGithub />
+        </a>
+      </div>
+      <div className="footer__copyright">
+        <small>
+          &copy; Done by Marcos Mujica(flamenow). All rights reserved. 2022
+        </small>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
